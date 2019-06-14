665af95cb33fea075171609a26418b73dda841bc
dengzhuli <dengzhuli@58ganji.com>
2019-06-14 20:17:53 +0800

    初始化

---
+++ WBBlades/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  WBBlades
+//
+//  Created by 邓竹立 on 2019/6/14.
+//  Copyright © 2019 邓竹立. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
