52033a6ec917726a2b064e9a952832bb66cf8999
dengzhuli <dengzhuli@58.com>
2019-12-19 16:37:59 +0800

    添加UI壳工程

---
+++ UIAPP/WBBlades/WBBlades/AppDelegate.m
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.m
+//  WBBlades
+//
+//  Created by 邓竹立 on 2019/12/19.
+//  Copyright © 2019 邓竹立. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+    // Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+    // Insert code here to tear down your application
+}
+
+
+@end
---
+++ UIAPP/WBBlades/WBBlades/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WBBlades
+//
+//  Created by 邓竹立 on 2019/12/19.
+//  Copyright © 2019 邓竹立. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface ViewController : NSViewController
+
+
+@end
+
