d1268cac21e2db10a8a986644a61f1a755c8312f
zengqinglong <zengqinglong@58.com>
2020-01-17 17:10:12 +0800

    添加注释

--- WBBlades/Model/WBBladesObjectHeader.h
+++ WBBlades/Model/WBBladesObjectHeader.h
@@ -6,6 +6,10 @@
 //  Copyright © 2019 邓竹立. All rights reserved.
 //
 
+/**
+ 对应Mach-O中的Object Header段信息
+ */
+
 #import <Foundation/Foundation.h>
 
 NS_ASSUME_NONNULL_BEGIN
@@ -13,12 +17,19 @@ NS_ASSUME_NONNULL_BEGIN
 @interface WBBladesObjectHeader : NSObject
 
 @property (nonatomic, copy) NSString *name;
+
 @property (nonatomic, copy) NSString *timeStamp;
+
 @property (nonatomic, copy) NSString *userID;
+
 @property (nonatomic, copy) NSString *groupID;
+
 @property (nonatomic, copy) NSString *mode;
+
 @property (nonatomic, copy) NSString *size;
+
 @property (nonatomic, copy) NSString *endHeader;
+
 @property (nonatomic, copy) NSString *longName;
 
 @property (nonatomic, assign) NSRange range;
